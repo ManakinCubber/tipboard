d83a8dd89ec6a9bfca7194f01b25b91e089c4beb
cedricmarx <cedric.marx@cubber.com>
2017-05-24 09:11:26 +0200

    Added info data reload

--- refresh-data.php
+++ refresh-data.php
@@ -8,6 +8,7 @@ if ($postdata['env'] === "production") {
         "api_key" => "API",
         "db_key" => "DATABASE",
         "front_key" => "FRONT",
+        "info_key" => "INFO",
     );
 } else {
     $env = array(
@@ -17,6 +18,7 @@ if ($postdata['env'] === "production") {
         "api_key" => "API_PREPROD",
         "db_key" => "DATABASE_PREPROD",
         "front_key" => "FRONT_PREPROD",
+        "info_key" => "INFO_PREPROD",
     );
 }
 
@@ -31,6 +33,10 @@ switch ($postdata['tile']) {
 
     case 'FRONT':
         reloadFront($env);
+        break;
+
+    case 'INFO':
+        reloadInfo($env);
         break;
 }
 
@@ -119,6 +125,32 @@ function reloadFront($env) {
                 "title"=> "Etat du front",
                 "description"=> $env['environnement'],
                 "just-value" => $state
+            )
+        )
+    );
+    updateData($data);
+}
+
+function reloadInfo($env) {
+    $ch = curl_init($env['api_url'] . "info");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    $result = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpcode !== 200) {
+        $result = "No data";
+    }
+    $data = array(
+        "tile" => "text",
+        "key" => $env['info_key'],
+        "data" =>json_encode(
+            array(
+                "text" => "<h2 id='INFO-title' class='result big-result fixed-height'>Informations</h2><h3 id='INFO-description' class='result label fixed-height'>" . $env['environnement'] . "</h3>" . $result
             )
         )
     );
